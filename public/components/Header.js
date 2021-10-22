46d7d91c1dbaf98082baa396084b10f3dae596aa
Hoang Vo <dev+g@hvvo.dev>
2021-10-22 17:55:21 +0700

    Fix seo and a11y issues

--- public/components/Header.js
+++ public/components/Header.js
@@ -4,8 +4,8 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <div class={styles.header}>
-      <a href="/" alt="Homepage">
-        <img src={logo} width={148} />
+      <a href="/">
+        <img src={logo} alt="Auralous" width={148} />
       </a>
     </div>
   );
--- public/pages/Home.js
+++ public/pages/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
     <div class="container">
       <div class={styles.first}>
         <div class={styles.logo}>
-          <img src={logo} />
+          <img src={logo} alt="Auralous" />
         </div>
         <p class={styles.motto}>Listen Together</p>
         <div class={styles.launches}>
@@ -22,11 +22,12 @@ const Home = () => {
           >
             Web App
           </a>
-          <a class={`${styles.launch} ${styles.launchNative}`}>
-            <img width={24} alt="Apple" src={apple} /> Coming soon
+          <a href="#" class={`${styles.launch} ${styles.launchNative}`}>
+            <img width={24} height={24} alt="Apple" src={apple} /> Coming soon
           </a>
-          <a class={`${styles.launch} ${styles.launchNative}`}>
-            <img width={24} alt="Android" src={android} /> Coming soon
+          <a href="#" class={`${styles.launch} ${styles.launchNative}`}>
+            <img width={24} height={24} alt="Android" src={android} /> Coming
+            soon
           </a>
         </div>
       </div>
